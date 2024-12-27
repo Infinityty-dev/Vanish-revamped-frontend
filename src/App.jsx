50f15f77b3d5dc53d6663927be099437a5331a29
okechukwu samuel okolie <okechukwu.okolie.2023@gmail.com>
2024-12-27 07:05:45 -0800

    location logic for the home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
 import './App.css'
-import Home from './pages/Home/Home.jsx'
+// import Home from './pages/Home/Home.jsx'
 
 import Header from './Component/Header.jsx'
 import Footer from './pages/Footer.jsx'
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 // // import SignUp from './Pages/Signup/Signup'
 import Team from './pages/Team.jsx'
+import Home from './pages/Home/Home.jsx'
 
 function App() {
 
@@ -15,13 +16,9 @@ function App() {
      <BrowserRouter>
          <Header/>
          <Routes>
-            {/* <Home/> */}
+          
             <Route path="/" element={<Home/>} />
-            
-            {/* <Route path="/SignUp" element={<SignUp />} /> */}
             <Route path="/about-us" element={<Team />} />
-
-
        </Routes>
        <Footer/>
        </BrowserRouter>
