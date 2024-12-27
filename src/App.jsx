b2e7911376f36e08867060704654509991deb49e
Techteemah <Fatee1bello@gmail.com>
2024-12-27 14:38:42 +0100

    Add OrderAssessment page

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom"
 // // import SignUp from './Pages/Signup/Signup'
 import Team from './pages/Team.jsx'
 import Home from './pages/Home/Home.jsx'
+import OrderAssessment from './pages/orderAssessment.jsx'
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
           
             <Route path="/" element={<Home/>} />
             <Route path="/about-us" element={<Team />} />
+            <Route path="/order-assessment" element={<OrderAssessment/>} />
        </Routes>
        <Footer/>
        </BrowserRouter>
