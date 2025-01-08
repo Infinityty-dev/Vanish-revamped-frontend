3ef30215ee95e6ba4908c3c43047ed5a88d41ec9
Techteemah <Fatee1bello@gmail.com>
2025-01-08 20:56:15 +0100

    orderAssessment

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,9 @@ import {BrowserRouter, Routes, Route} from "react-router-dom"
 // // import SignUp from './Pages/Signup/Signup'
 import Team from './pages/Team.jsx'
 import Home from './pages/Home/Home.jsx'
-import OrderAssessment from './pages/OrderAssessment.jsx'
 import OwnVan from './pages/ownVan.jsx'
 import NeedVan from './pages/NeedVan.jsx'
 import OptionPage from './pages/OptionPage.jsx'
-import OrderAssessment from './pages/orderAssessment.jsx'
 import SignUp from './Component/SignUp.jsx'
 import SignIn from './Component/SignIn.jsx'
 import DriversSignUp from './Component/DriversSignUp.jsx'
@@ -22,6 +20,7 @@ import DriverTerms from './Component/DriverTerms.jsx'
 import Movement from "./Component/Movement.jsx";
 import Location from "./Component/Location.jsx"
 import DriversDashboard from './Component/DriversDashboard.jsx'
+import OrderAssessment from './pages/orderAssessment.jsx'
 
 
 function App() {
@@ -34,13 +33,9 @@ function App() {
           
             <Route path="/" element={<Home/>} />
             <Route path="/about-us" element={<Team />} />
-
-            <Route path="/orderAssessment" element={<OrderAssessment/>} />
             <Route path="optionpage" element={<OptionPage/>} />
             <Route path="ownvan" element={<OwnVan/>} />
             <Route path="needvan" element={<NeedVan/>} />
-            
-            <Route path="/order-assessment" element={<OrderAssessment/>} />
             <Route path="/SignUp" element={<SignUp/>} />
             <Route path='/Terms' element={<Terms/>} />
             <Route path="/SignIn" element={<SignIn/>} />
@@ -49,7 +44,7 @@ function App() {
             <Route path="/DriversSignIn" element={<DriversSignIn/>} />
             <Route path="/Movement" element={<Movement/>} />
             <Route path="/Location" element={<Location/>} />
-
+            <Route path="/OrderAssessment" element ={<OrderAssessment/>}/>
             <Route path="/DriversDashboard" element={<DriversDashboard/>} />
           
             
